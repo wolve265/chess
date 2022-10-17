0fd6d2f0a9e02df584e53748c5a57f0e9a70aca6
Krzysztof Cislo <krzysztof.cislo265@gmail.com>
2022-10-17 20:39:10 +0200

    added python 3.10 guard

--- chess_runner.py
+++ chess_runner.py
@@ -5,8 +5,8 @@ from chess import Chess
 
 
 if __name__ == "__main__":
-    if not sys.version_info >= (3, 8):
-        print("This chess app requires Python 3.8 or newer.")
+    if not sys.version_info >= (3, 10):
+        print("This chess app requires Python 3.10 or newer.")
         sys.exit(1)
     chess = Chess()
     chess.run()
